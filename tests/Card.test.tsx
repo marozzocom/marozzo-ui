2e078e0e48801754b12821491e1af6965c59db8b
Ilkka Hartikainen <ilkka.hartikainen@gmail.com>
2019-12-20 22:49:39 +0200

    test(card tests): update card tests

--- tests/Card.test.tsx
+++ tests/Card.test.tsx
@@ -1,8 +1,13 @@
 import React from "react"
 import { Card } from "../src/card/Card"
 import { render } from "@testing-library/react"
+import { ThemeProvider, defaultTheme } from "../src/theme"
+
+const renderComponent = () => render(<ThemeProvider theme={defaultTheme}>
+  <Card>Card</Card>
+</ThemeProvider>)
 
 test("Renders Card", () => {
-  const { getByText } = render(<Card>Card</Card>)
+  const { getByText } = renderComponent()
   expect(getByText("Card")).not.toBeNull()
 })

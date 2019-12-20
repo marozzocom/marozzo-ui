99f6e72bc8884c624bc20d6bd16d533ab78be515
Ilkka Hartikainen <ilkka.hartikainen@gmail.com>
2019-12-20 22:52:59 +0200

    feat(drawer): modality, overlay, focusTrap
    Add support for custom aniumations, modality and focusTrap (to be extracted as a hook later for
    reuse). Also default animation moved to component and added support for positioning.

--- src/drawer/Drawer.tsx
+++ src/drawer/Drawer.tsx
@@ -1,23 +1,122 @@
-import React, { FC } from "react"
-import { Box } from "../box/Box"
+import React, { FC, ComponentProps, useRef, useEffect, useState } from "react"
 import { Portal } from "../portal/Portal"
 import { Disclosure } from "../disclosure/Disclosure"
-import { useTheme } from "emotion-theming"
-import { Theme } from "../theme/models"
 import nanoid from "nanoid"
+import { Surface } from "../surface/Surface"
+import { MotionProps } from "framer-motion"
+import { Overlay } from "../overlay/Overlay"
+import { Close } from "../close/Close"
 
 interface Props {
   open?: boolean
   key?: string
+  far?: boolean
+  animation?: MotionProps
+  modal?: boolean
+  overlayProps?: ComponentProps<typeof Overlay>
+  close?: () => void
 }
 
-export const Drawer: FC<Props> = ({ open = true, key=nanoid(), children }) => {
-  const theme = useTheme<Theme>()
-  
+export const Drawer: FC<Props> = ({ open = true, key = nanoid(), far, animation, children, overlayProps, modal = true, close }) => {
+  const focusTrapRef = useRef(null)
+  const [focusTrap, setFocusTrap] = useState({ first: undefined, last: undefined })
+
+  const motionProps: MotionProps = {
+    initial: { opacity: 0, x: `${far ? 100 : -100}%`, position: "fixed", left: !far && 0, right: far && 0, top: 0, bottom: 0 },
+    animate: { opacity: 1, x: 0 },
+    exit: { opacity: 0, x: `${far ? 100 : -100}%` },
+    transition: {
+      type: "tween",
+      duration: .3
+    },
+    ...animation
+  }
+
+  const handleTab = (event: KeyboardEvent) => {
+    console.log("press")
+    event.key === "Tab" && !focusTrapRef.current.contains(event.currentTarget) && event.shiftKey ? focusTrap.last.focus() : focusTrap.first.focus()
+  }
+
+  useEffect(() => {
+    if (focusTrapRef.current) {
+
+      const focusable = focusTrapRef.current.querySelectorAll('button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])');
+      setFocusTrap({
+        first: focusable[0],
+        last: focusable[focusable.length - 1]
+      })
+
+      if (!focusTrap.first) return
+
+      focusTrap.first.focus()
+      window.addEventListener("keydown", handleTab)
+    }
+    return () => {
+      window.removeEventListener("keydown", handleTab)
+    }
+  }, [focusTrapRef.current])
+
   return (
-  <Portal>
-    <Disclosure animation={theme.disclosures.drawer}>
-      {open && <Box width="6" boxShadow="normal" key={key}>{children}</Box>}
-    </Disclosure>
-  </Portal>
-)}
+    <Portal>
+      {modal && open && <Overlay {...overlayProps} />}
+      <Disclosure animation={motionProps}>
+        {open && <DrawerContainer key={key} close={close}>{children}</DrawerContainer>}
+      </Disclosure>
+    </Portal>
+  )
+}
+
+
+const DrawerContainer: FC<{ close: () => void }> = ({ children, close }) => {
+  const focusTrapRef = useRef(null)
+  const [focusTrap, setFocusTrap] = useState({ first: undefined, last: undefined })
+
+  const handleTab = (event: KeyboardEvent) => {
+    if (event.key !== "Tab") {
+      return
+    }
+    if (!focusTrapRef.current.contains(event.target)) {
+      focusTrap.first.focus()
+      event.preventDefault()
+    }
+    if (event.target === focusTrap.first && event.shiftKey) {
+      focusTrap.last.focus()
+    }
+    if (event.target === focusTrap.last) {
+      focusTrap.first.focus()
+      event.preventDefault()
+    }
+  }
+
+  useEffect(() => {
+    if (!focusTrapRef.current) {
+      return
+    }
+
+    const focusable = focusTrapRef.current.querySelectorAll(`button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])`)
+
+    if (focusable.length === 0) {
+      return
+    }
+
+    setFocusTrap({
+      first: focusable[0],
+      last: focusable[focusable.length - 1]
+    })
+    
+    focusable[0].focus()
+
+    window.addEventListener("keydown", handleTab)
+
+    return () => {
+      window.removeEventListener("keydown", handleTab)
+    }
+  }, [focusTrapRef.current])
+
+  return (
+    <Surface ref={focusTrapRef} width="7" boxShadow="subtle">
+      {close && <Close onClick={close} />}
+      {children}
+    </Surface>
+  )
+}
--- stories/Drawer.stories.tsx
+++ stories/Drawer.stories.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react"
 import { Drawer } from "../src/drawer/Drawer";
 import { Button } from "../src/button/Button";
+import { Link } from "../src/link/Link";
 
 export default {
   component: Drawer,
@@ -10,7 +11,78 @@ export default {
 export const drawer = () => {
   const [open, setOpen] = useState(false)
   return <>
-    <Button onClick={() => setOpen(!open)} id="drawer-toggle">toggle</Button>
+    <Button position="fixed" right="2" zIndex={1} onClick={() => setOpen(!open)} id="drawer-toggle">toggle</Button>
     <Drawer open={open}>drawer</Drawer>
+  </>
+}
+
+export const drawerWithFocusTrap = () => {
+  const [open, setOpen] = useState(false)
+  return <>
+    <Button position="fixed" right="2" zIndex={1} onClick={() => setOpen(!open)} id="drawer-toggle">toggle</Button>
+    <Drawer open={open}>
+      <>
+        <Link to="http://google.com" newTab>Link 1</Link><br />
+        <Link to="http://google.com" newTab>Link 2</Link><br />
+        <Link to="http://google.com" newTab>Link 3</Link><br />
+        <Link to="http://google.com" newTab>Link 4</Link><br />
+        <Link to="http://google.com" newTab>Link 5</Link><br />
+        <Link to="http://google.com" newTab>Link 6</Link><br />
+        <Link to="http://google.com" newTab>Link 7</Link><br />
+        <Link to="http://google.com" newTab>Link 8</Link><br />
+        <Link to="http://google.com" newTab>Link 9</Link><br />
+        <Link to="http://google.com" newTab>Link 10</Link>
+      </>
+    </Drawer>
+  </>
+}
+
+export const drawerWithCloseButton = () => {
+  const [open, setOpen] = useState(false)
+  return <>
+    <Button position="fixed" right="2" zIndex={1} onClick={() => setOpen(!open)} id="drawer-toggle">toggle</Button>
+    <Drawer open={open} close={() => setOpen(false)}>drawer</Drawer>
+  </>
+}
+
+export const modelessDrawer = () => {
+  const [open, setOpen] = useState(false)
+  return <>
+    <Button position="fixed" right="2" onClick={() => setOpen(!open)} id="drawer-toggle">toggle</Button>
+    <Drawer open={open} modal={false}>drawer</Drawer>
+  </>
+}
+
+export const drawerWithCustomOverlay = () => {
+  const [open, setOpen] = useState(false)
+  return <>
+    <Button position="fixed" right="2" zIndex={1} onClick={() => setOpen(!open)} id="drawer-toggle">toggle</Button>
+    <Drawer open={open} overlayProps={{ bg: "tomato" }}>drawer</Drawer>
+  </>
+}
+
+export const drawerPositionedFar = () => {
+  const [open, setOpen] = useState(false)
+  return <>
+    <Button position="fixed" left="2" zIndex={1} onClick={() => setOpen(!open)} id="drawer-toggle">toggle</Button>
+    <Drawer open={open} far={true}>drawer</Drawer>
+  </>
+}
+
+export const drawerWithCustomAnimation = () => {
+  const [open, setOpen] = useState(false)
+  return <>
+    <Button position="fixed" right="2" zIndex={1} onClick={() => setOpen(!open)} id="drawer-toggle">toggle</Button>
+    <Drawer open={open} animation={
+      {
+        initial: { opacity: 0, y: "-100%", position: "fixed", left: 0, top: 0, bottom: 0 },
+        animate: { opacity: 1, y: 0 },
+        exit: { opacity: 0, y: "100%" },
+        transition: {
+          type: "spring",
+          duration: .3
+        }
+      }
+    }>drawer</Drawer>
   </>
 }

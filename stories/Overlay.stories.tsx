34aa2d66d567b801ca680b9863187c1f98bc6cbd
Ilkka Hartikainen <ilkka.hartikainen@gmail.com>
2019-12-20 22:53:53 +0200

    feat(overlay): new overlay component

---
+++ stories/Overlay.stories.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+import { Overlay } from "../src/overlay/Overlay";
+
+export default {
+  component: Overlay,
+  title: "Overlay",
+};
+
+export const overlay = () => <Overlay />

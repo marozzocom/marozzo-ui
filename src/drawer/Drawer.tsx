482a9983a612e704437cbb53e82bd56edbd36a53
Ilkka Hartikainen <ilkka.hartikainen@gmail.com>
2019-12-21 15:12:37 +0200

    improvement(focustrap): extract to hook
    Extract focusTrap to useFocusTrap hook, that can be used by importing a ref from the hook and
    placing that to an object. This commit also fixes bugs in focus trap tab handling skipping the last
    element with shift tab.
    
    fix #3

--- src/drawer/Drawer.tsx
+++ src/drawer/Drawer.tsx
@@ -6,6 +6,7 @@ import { Surface } from "../surface/Surface"
 import { MotionProps } from "framer-motion"
 import { Overlay } from "../overlay/Overlay"
 import { Close } from "../close/Close"
+import { useFocusTrap } from "../focusTrap/useFocusTrap"
 
 interface Props {
   open?: boolean
@@ -19,9 +20,6 @@ interface Props {
 }
 
 export const Drawer: FC<Props> = ({ open = true, key = nanoid(), far, hasCloseButton=true, animation, children, overlayProps, modal = true, close }) => {
-  const focusTrapRef = useRef(null)
-  const [focusTrap, setFocusTrap] = useState({ first: undefined, last: undefined })
-
   const motionProps: MotionProps = {
     initial: { opacity: 0, x: `${far ? 100 : -100}%`, position: "fixed", left: !far && 0, right: far && 0, top: 0, bottom: 0 },
     animate: { opacity: 1, x: 0 },
@@ -32,27 +30,6 @@ export const Drawer: FC<Props> = ({ open = true, key = nanoid(), far, hasCloseBu
     },
     ...animation
   }
-
-  const handleTab = (event: KeyboardEvent) => event.key === "Tab" && !focusTrapRef.current.contains(event.currentTarget) && event.shiftKey ? focusTrap.last.focus() : focusTrap.first.focus()
-
-  useEffect(() => {
-    if (focusTrapRef.current) {
-
-      const focusable = focusTrapRef.current.querySelectorAll('button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])');
-      setFocusTrap({
-        first: focusable[0],
-        last: focusable[focusable.length - 1]
-      })
-
-      if (!focusTrap.first) return
-
-      focusTrap.first.focus()
-      window.addEventListener("keydown", handleTab)
-    }
-    return () => {
-      window.removeEventListener("keydown", handleTab)
-    }
-  }, [focusTrapRef.current])
 
   return (
     <Portal>
@@ -66,53 +43,10 @@ export const Drawer: FC<Props> = ({ open = true, key = nanoid(), far, hasCloseBu
 
 
 const DrawerContainer: FC<{ close: () => void }> = ({ children, close }) => {
-  const focusTrapRef = useRef(null)
-  const [focusTrap, setFocusTrap] = useState({ first: undefined, last: undefined })
-
-  const handleTab = (event: KeyboardEvent) => {
-    if (event.key !== "Tab") {
-      return
-    }
-    if (!focusTrapRef.current.contains(event.target)) {
-      focusTrap.first.focus()
-      event.preventDefault()
-    }
-    if (event.target === focusTrap.first && event.shiftKey) {
-      focusTrap.last.focus()
-    }
-    if (event.target === focusTrap.last) {
-      focusTrap.first.focus()
-      event.preventDefault()
-    }
-  }
-
-  useEffect(() => {
-    if (!focusTrapRef.current) {
-      return
-    }
-
-    const focusable = focusTrapRef.current.querySelectorAll(`button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])`)
-
-    if (focusable.length === 0) {
-      return
-    }
-
-    setFocusTrap({
-      first: focusable[0],
-      last: focusable[focusable.length - 1]
-    })
-    
-    focusable[0].focus()
-
-    window.addEventListener("keydown", handleTab)
-
-    return () => {
-      window.removeEventListener("keydown", handleTab)
-    }
-  }, [focusTrapRef.current])
+  const focusTrap = useFocusTrap()
 
   return (
-    <Surface ref={focusTrapRef} width="7" boxShadow="subtle">
+    <Surface ref={focusTrap} width="6" boxShadow="subtle">
       {close && <Close onClick={close} />}
       {children}
     </Surface>
---
+++ src/focusTrap/useFocusTrap.tsx
@@ -0,0 +1,51 @@
+import React, { useRef, useState, useEffect } from "react"
+
+export const useFocusTrap = () => {
+  const focusTrap = useRef(null)
+  const [focusTrapZone, setFocusTrapZone] = useState({ first: undefined, last: undefined })
+
+  const handleTab = (event: KeyboardEvent) => {
+    if (event.key !== "Tab") {
+      return
+    }
+    if (!focusTrap.current.contains(event.target)) {
+      focusTrapZone.first.focus()
+      event.preventDefault()
+    }
+    if (event.target === focusTrapZone.first && event.shiftKey) {
+      focusTrapZone.last.focus()
+      event.preventDefault()
+    }
+    if (event.target === focusTrapZone.last && !event.shiftKey) {
+      focusTrapZone.first.focus()
+      event.preventDefault()
+    }
+  }
+
+  useEffect(() => {
+    if (!focusTrap.current) {
+      return
+    }
+
+    const focusableElements = focusTrap.current.querySelectorAll(`button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])`)
+
+    if (focusableElements.length === 0) {
+      return
+    }
+
+    setFocusTrapZone({
+      first: focusableElements[0],
+      last: focusableElements[focusableElements.length - 1]
+    })
+    
+    focusableElements[0].focus()
+
+    window.addEventListener("keydown", handleTab)
+
+    return () => {
+      window.removeEventListener("keydown", handleTab)
+    }
+  }, [focusTrap.current])
+
+  return focusTrap
+}

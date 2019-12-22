84038f2fad0df4771691a66201c3a40b5d0b1381
Ilkka Hartikainen <ilkka.hartikainen@gmail.com>
2019-12-22 22:43:13 +0200

    feat: add content section

---
+++ src/contentSection/ContentSection.tsx
@@ -0,0 +1,27 @@
+import React, { FC, useRef, useEffect } from "react"
+import { Box } from "../box/Box"
+import { useToc } from "../toc"
+
+interface Props {
+  name: string
+}
+
+export const ContentSection: FC<Props> = ({ name, children }) => {
+  const { observer, setToc } = useToc()
+  const ref = useRef<HTMLHeadingElement>()
+
+  if (observer) {
+    useEffect(() => {
+      observer.observe(ref.current)
+      setToc((toc: any) => [
+        ...toc,
+        {
+          name,
+          element: ref.current
+        }
+      ])
+    }, [])
+  }
+
+  return <Box ref={ref}>{children}</Box>
+}

3c9486075be9a7c99a7c9fb23853369444b3e5a7
Ilkka Hartikainen <ilkka.hartikainen@gmail.com>
2019-12-22 05:16:17 +0200

    fix(button): fix passing styles

--- src/button/Button.tsx
+++ src/button/Button.tsx
@@ -3,8 +3,8 @@ import { jsx } from "@emotion/core"
 import { Box } from "../box/Box"
 import { FC, forwardRef, ComponentProps } from "react"
 
-export const Button: FC<ComponentProps<typeof Box>> = forwardRef(({ ...props }, ref) => (
-  <Box style={{ border: "none" }} bg="primary" p={2} borderRadius="normal" as="button" ref={ref} {...props} />
+export const Button: FC<ComponentProps<typeof Box>> = forwardRef(({ style, ...props }, ref) => (
+  <Box style={{ border: "none", ...style }} bg="primary" p={2} borderRadius="normal" as="button" ref={ref} {...props} />
 ))
 
 Button.displayName = "Button"

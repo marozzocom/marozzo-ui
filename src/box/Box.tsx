4b38938e3d75de3baf4fefd941b0a601be8dd493
Ilkka Hartikainen <ilkka.hartikainen@gmail.com>
2019-12-29 03:20:42 +0200

    feat(box): add support for variant
    New variant prop for easily overriding defaults styles of components or setting a different look on
    them.

--- src/box/Box.tsx
+++ src/box/Box.tsx
@@ -1,22 +1,24 @@
 /** @jsx jsx */
-import { jsx, CSSObject } from "@emotion/core"
+import { jsx } from "@emotion/core"
 
 // TODO: The typings are not correct
 
 import { FC } from "react"
 import { useTheme } from "../theme"
 import facepaint from "facepaint"
+
 interface HTMLProps<T> extends React.RefAttributes<T>, Omit<React.HTMLAttributes<T>, "style"> {
   target?: string
   as?: React.ElementType
   style?: facepaint.Arg
   innerRef?: React.MutableRefObject<T>
+  variant?: facepaint.Arg
   onClick?: (event: any) => void
 }
 
 interface Props<T = HTMLElement> extends HTMLProps<T> {}
 
-export const Box: FC<Props<HTMLElement>> = ({ as = "div", style, children, innerRef, ...props }) => {
+export const Box: FC<Props<HTMLElement>> = ({ as = "div", style, variant, children, innerRef, ...props }) => {
   const Tag: any = `${as}`
   const { breakpoints } = useTheme()
 
@@ -28,6 +30,7 @@ export const Box: FC<Props<HTMLElement>> = ({ as = "div", style, children, inner
         position: "relative",
         minWidth: 0,
         pointerEvents: "all",
+        ...variant,
         ...style
       })}
       {...props}>

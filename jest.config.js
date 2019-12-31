e02e162835cb37dc57eacffb7d0949366bd38d4a
Ilkka Hartikainen <ilkka.hartikainen@gmail.com>
2019-12-31 13:44:36 +0200

    chore: format jest config

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,8 @@ module.exports = {
   preset: "ts-jest",
   globals: {
     "ts-jest": {
-      diagnostics: false,
-    },
+      diagnostics: false
+    }
   },
   testEnvironment: "jsdom",
   moduleDirectories: ["node_modules", "src"],

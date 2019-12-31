a6a54b902e9ad7f16f798722237a4e5b7b74f565
Ilkka Hartikainen <ilkka.hartikainen@gmail.com>
2019-12-31 03:24:13 +0200

    improvement(toc): add a `resetToc` function for clearing TOC contents

--- src/toc/useToc.tsx
+++ src/toc/useToc.tsx
@@ -4,9 +4,9 @@ import { TocContext } from "./TocProvider"
 export const useToc = () => {
   try {
     const { toc, setToc, observer, active } = useContext(TocContext)
-
-    return { toc, setToc, observer, active }
+    const resetToc = () => setToc([])
+    return { toc, setToc, observer, active, resetToc }
   } catch (error) {
-    return { toc: null, setToc: null, observer: null, active: null }
+    return { toc: null, setToc: null, observer: null, active: null, resetToc: null }
   }
 }

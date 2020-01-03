f2e0ff10b0c9eba16aaa74a7d94e6b7615e35b8f
Ilkka Hartikainen <ilkka.hartikainen@gmail.com>
2020-01-03 04:23:06 +0200

    improvement(scrollprogress): now calculates exit style on each render
    Makes sure memoized effect will not be erroneously applied

--- src/scrollProgress/ScrollProgress.tsx
+++ src/scrollProgress/ScrollProgress.tsx
@@ -10,24 +10,22 @@ interface Props extends ComponentProps<typeof Progress>, FixtureProps {}
 export const ScrollProgress: FC<Props> = ({ type = "fixed", vertical = Vertical.Top, horizontal, ...props }) => {
   const { progress, showProgress } = useScrollProgress()
 
-  const animation = useMemo(
-    () => ({
-      initial: { opacity: 0, x: 0 },
-      animate: { opacity: 1, x: 0 },
-      exit: progress === 100 ? { x: "100%" } : { opacity: 0 },
-      transition: {
-        type: "tween",
-        duration: 0.6
-      }
-    }),
-    [showProgress]
-  )
+  const animation = {
+    initial: { opacity: 0, x: 0 },
+    animate: { opacity: 1, x: 0 },
+    exit: progress > 50 ? { x: "100%" } : { opacity: 0 },
+    transition: {
+      type: "tween",
+      duration: 0.6
+    }
+  }
 
   return (
     <Fixture type={type} vertical={vertical} horizontal={horizontal}>
       <Box
         style={{
-          width: "100%"
+          width: "100%",
+          overflow: "hidden"
         }}>
         <Disclosure animation={animation}>{showProgress && <Progress value={progress} {...props} />}</Disclosure>
       </Box>

597f301b5c817dcf356dc7f2ed77191a8360e981
Ilkka Hartikainen <ilkka.hartikainen@gmail.com>
2020-02-11 21:34:12 +0200

    fix: fix progress provider

--- src/progress/ProgressProvider.tsx
+++ src/progress/ProgressProvider.tsx
@@ -7,14 +7,14 @@ export interface Operations {
 
 const ProgressContext = createContext<{
   operations: Operations
-  start: (operations: Operations) => void
+  start: (id: string) => void
   stop: (id: string) => void
 }>(null)
 
 const ProgressProvider: FC<{}> = ({ children }) => {
   const [operations, setOperations] = useState<Operations>({})
 
-  const start = useCallback(operations => setOperations(currentOperations => ({ ...currentOperations, ...operations })), [])
+  const start = useCallback(id => setOperations(currentOperations => ({ ...currentOperations, [id]: true })), [])
   const stop = useCallback(id => setOperations(currentOperations => ({ ...dissociate(id)(currentOperations) })), [])
 
   return <ProgressContext.Provider value={{ operations, start, stop }}>{children}</ProgressContext.Provider>
